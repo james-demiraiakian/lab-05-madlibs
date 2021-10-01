0caff7c6791e508d50080e9ce905d695be1bf93b
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-01 19:58:58 -0400

    imported all documents

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 // import functions and grab DOM elements
 
+const nameField = document.getElementById('name-field');
+const verbField = document.getElementById('verb-field');
+const pronounField = document.getElementById('pronoun-field');
+const locaField = document.getElementById('loca-field');
+const nounField = document.getElementById('noun-field');
+const pronounField2 = document.getElementById('pronoun2-field');
+const pronounField3= document.getElementById('pronoun3-field');
+const pronounField4 = document.getElementById('pronoun4-field');
+const nounField2 = document.getElementById('noun2-field');
+const pronounField5 = document.getElementById('pronoun5-field');
+const verbField2 = document.getElementById('verb2-field');
+const pronounField6 = document.getElementById('pronoun6-field');
+const nounField3 = document.getElementById('noun3-field');
+
+const revealStory = document.getElementById('reveal-button');
+
+
+
 // initialize global state
 
 // set event listeners 
