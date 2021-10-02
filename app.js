5ab277ad9c6da1905c74fc2a646a44e727da2bf5
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-01 17:27:19 -0700

    added my event listener

--- app.js
+++ app.js
@@ -16,11 +16,69 @@ const nounField3 = document.getElementById('noun3-field');
 
 const revealStory = document.getElementById('reveal-button');
 
+const name = document.getElementById('name');
+const verb = document.getElementById('verb');
+const pronoun = document.getElementById('pronoun');
+const loca = document.getElementById('loca');
+const noun = document.getElementById('noun');
+const pronoun2 = document.getElementById('pronoun2');
+const pronoun3 = document.getElementById('pronoun3');
+const pronoun4 = document.getElementById('pronoun4');
+const noun2 = document.getElementById('noun2');
+const pronoun5 = document.getElementById('pronoun5');
+const verb2 = document.getElementById('verb2');
+const pronoun6 = document.getElementById('pronoun6');
+const noun3 = document.getElementById('noun3');
+
 
 
 // initialize global state
 
 // set event listeners 
+
+revealStory.addEventListener('click', () => {
+    let newName = nameField.value;
+    name.textContent = newName;
+    
+    let newVerb = verbField.value;
+    verb.textContent = newVerb;
+    
+    let newPronoun = pronounField.value;
+    pronoun.textContent = newPronoun;
+
+    let newLoca = locaField.value;
+    loca.textContent = newLoca;
+
+    let newNoun = nounField.value;
+    noun.textContent = newNoun;
+
+    let newPronoun2 = pronounField2.value;
+    pronoun2.textContent = newPronoun2;
+
+    let newPronoun3 = pronounField3.value;
+    pronoun3.textContent = newPronoun3;
+
+    let newPronoun4 = pronounField4.value;
+    pronoun4.textContent = newPronoun4;
+
+    let newNoun2 = nounField2.value;
+    noun2.textContent = newNoun2;
+
+    let newPronoun5 = pronounField5.value;
+    pronoun5.textContent = newPronoun5;
+
+    let newVerb2 = verbField2.value;
+    verb2.textContent = newVerb2;
+
+    let newPronoun6 = pronounField6.value;
+    pronoun6.textContent = newPronoun6;
+
+    let newNoun3 = nounField3.value;
+    noun3.textContent = newNoun3;
+
+  });
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
